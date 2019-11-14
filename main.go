caf008b62fd0dc9ceaae42b9903673e20fa16776
Ilia <reistlin87@yandex.ru>
2019-11-14 23:15:38 +0200

    first commit

--- main.go
+++ main.go
@@ -7,10 +7,15 @@ import (
 
 func main() {
 	e := echo.New()
+
 	e.GET("/", func(c echo.Context) error {
 		return c.String(http.StatusOK, "Hello, World!")
 	})
+
 	h := &handler{}
-	e.POST("/login", h.login)
-	e.Logger.Fatal(e.Start(":1323"))
+	e.POST("/token", h.login)
+
+	e.GET("/pub", h.sharePub)
+
+	e.Logger.Fatal(e.Start(":8080"))
 }
